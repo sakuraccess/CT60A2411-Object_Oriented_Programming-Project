c71f4e1fa836f0330eefb82a97e2d32e48245af3
sakuraccess <115935262+sakuraccess@users.noreply.github.com>
2023-04-11 21:21:32 +0300

    first text

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     private static final String MENU =
             """
-                    Welcome to the LUT Kino at Yliopistonkatu!
                     1. Add a movie
                     2. Remove a movie
                     3. View a movie
@@ -18,10 +17,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Theater cinema = new Theater("LUT Kino", "Yliopistonkatu");
+
         Scanner input = new Scanner(System.in);
         int option = -1;
 
         while (true) {
+            System.out.printf("Welcome to the %s at %s!\n", cinema.getName(), cinema.getLocation());
             System.out.println(MENU);
             System.out.print("Enter your choice:");
 
@@ -29,7 +31,7 @@ public class Main {
 
             switch (option) {
                 case 1: {
-
+                    break;
                 }
             }
 
